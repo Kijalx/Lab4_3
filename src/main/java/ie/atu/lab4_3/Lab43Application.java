9fbd380ee09bb4972181933f21f7548e1cdc3b26
Aleks <G00396018@atu.ie>
2023-10-11 10:09:41 +0100

    Setup Project

---
+++ src/main/java/ie/atu/lab4_3/Lab43Application.java
@@ -0,0 +1,13 @@
+package ie.atu.lab4_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab43Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab43Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab4_3/Lab43ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab4_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab43ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
